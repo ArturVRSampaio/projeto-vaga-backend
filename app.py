407cbb2a1e74ef0a305e95e7d00aa90130769fc1
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2022-02-20 15:25:22 -0300

    swagger

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app, title="ACMEVita api", description="ACMEVita api ")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@api.route('/example/')
+class hello_world(Resource):
+    def get(self):
+        return {'hi': 'yeah'},200
+    def post(self):
+        return {'hi': 'yeah'},201
+    def put(self):
+        return {'hi': 'yeah'},404
+    def delete(self):
+        return {'hi': 'yeah'},403
+    def patch(self):
+        return {'hi': 'yeah'},500
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
